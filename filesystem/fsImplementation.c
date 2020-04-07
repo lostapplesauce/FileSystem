e4928feaef23e1be2dceca69308fd8ac61b5f006
Pedro Souto <Pedro.Souto.SFSU@gmail.com>
2020-04-07 13:56:29 -0700

    Initial project commit

---
+++ filesystem/fsImplementation.c
@@ -0,0 +1,6 @@
+// File created by Pedro Souto - This file WAS NOT given to us by the professor
+// This is the file where we will implement all the functionalities professor asked
+// From instructions: The driver should be interactive (with all built in commands) to list directories, create directories, add and remove files, copy files, move files, setting meta data, and two “special commands” one to copy from the normal filesystem to your filesystem and the other from your filesystem to the normal filesystem
+
+#include "fsImplementation.h"
+
---
+++ filesystem/fsImplementation.h
@@ -0,0 +1,34 @@
+// File created by Pedro Souto - This file WAS NOT given to us by the professor
+// Header file for fsImplementation.c
+
+#ifndef fsImplementation_h
+#define fsImplementation_h
+
+#include <stdio.h>
+
+/*
+ HEADER PROTOTYPES FOR FILE SYSTEM FUNCTIONALITIES
+ ALL FUNCTIONS PROTYPES WILL GO HERE
+ ALL FUNCTIONS IMPLEMENTATIONS WILL GO IN .c FILE
+*/
+
+// List directories
+
+// Create directory
+
+// Add file
+
+// Remove file
+
+// Cope file
+
+// Move file
+
+// Set metadata
+
+// Copy from the normal filesystem to your filesystem
+
+// Cope from your filesystem to the normal filesystem
+
+
+#endif /* fsImplementation_h */
